0602c80dc199cac48c93c2d0dd7389ef6d69a250
kavikkannan <kavikkannan.k@gmail.com>
2024-02-23 10:09:18 +0530

    23/02/10:10pm

--- app/(main)/thank/page.jsx
+++ app/(main)/thank/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Selection from '@/components/Selection';
+import Thank from '@/components/thank';
 import Commonheader from '@/components/Common_Header';
 import Lottie from 'lottie-react';
 import under from '@/assests/under.json'
@@ -8,12 +8,12 @@ export default function thank() {
     return (
         <div>
              <Commonheader/>
-             
-              <div className='flex flex-col justify-center items-center h-screen w-full text-2xl sm:flex-row '>
+             <Thank/>
+              {/* <div className='flex flex-col justify-center items-center h-screen w-full text-2xl sm:flex-row '>
               <h1>website under construction</h1>
         <Lottie animationData={under} className=' h-1/2 w-1/2'/>
-
-        </div>
+ 
+        </div>*/}
         </div>
       
     )
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import Selection from '@/components/Selection';
+import Landing from '@/components/Landing_Page';
 import Commonheader from '@/components/Common_Header';
 import Lottie from 'lottie-react';
 import under from '@/assests/under.json'
@@ -8,12 +8,12 @@ export default function Main() {
     return (
         <div>
              <Commonheader/>
-             
-              <div className='flex flex-col justify-center items-center h-screen w-full text-2xl sm:flex-row '>
+             <Landing/>
+              {/* <div className='flex flex-col justify-center items-center h-screen w-full text-2xl sm:flex-row '>
               <h1>website under construction</h1>
         <Lottie animationData={under} className=' h-1/2 w-1/2'/>
 
-        </div>
+        </div> */}
         </div>
       
     )
