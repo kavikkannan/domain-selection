93c3c3e11db8d0bc2a15e74ed6510e077881384a
kavikkannan <kavikkannan.k@gmail.com>
2024-03-11 21:11:56 +0530

    j

--- components/LandingComponents/SignInButton.jsx
+++ components/LandingComponents/SignInButton.jsx
@@ -35,7 +35,7 @@ const SignInButton = () => {
                     // Check if the signed-in email is in the user data file
                     const userDataWithEmail = userData.users.find((user) => user.EmailId === signedInEmail);
                     if (userDataWithEmail) {
-                        router.push('/show_mem');
+                        router.push('/Domain_Selection');
                     } else {
                         // If the user email is not found, set error message
                         setLoading(false);
